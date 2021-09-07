0ec870998d7d58fd6c8f3667c5aa929acdbea501
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-09-07 08:19:02 +0200

    webdrivery

--- src/main/java/browserSelectCase.java
+++ src/main/java/browserSelectCase.java
@@ -9,26 +9,26 @@ public class browserSelectCase {
 
     @Test
     public void openBrowser() {
-        WebDriver driver = getDriver("chrome");
+        WebDriver driver = getDriver("edge");
     }
 
     public WebDriver getDriver(String browser) {
         switch (browser) {
             case "chrome":
-                String pathC = "D:\\Szkolenie_tester\\Chromedriver93/chromedriver.exe";
-                System.setProperty("webdriver.chrome.driver", pathC);
+               // String pathC = "D:\\Szkolenie_tester\\Chromedriver93\\chromedriver.exe";
+                //System.setProperty("webdriver.chrome.driver", pathC);
                 //WebDriver driverChrome = new ChromeDriver();
                 //driverChrome.get("https://www.google.com");
                 return new ChromeDriver();
             case "firefox":
-                String pathF = "D:\\Szkolenie_tester\\Firefoxdriver\\geckodriver.exe";
-                System.setProperty("webdriver.gecko.driver", pathF);
+                //String pathF = "D:\\Szkolenie_tester\\Firefoxdriver\\geckodriver.exe";
+                //System.setProperty("webdriver.gecko.driver", pathF);
                 //WebDriver driverFirefox = new FirefoxDriver();
                 //driverFirefox.get("https://www.google.com");
                 return new FirefoxDriver();
             case "edge":
-                String pathE = "D:\\Szkolenie_tester\\Edgedriver\\msedgedriver.exe";
-                System.setProperty("webdriver.edge.driver", pathE);
+                //String pathE = "D:\\Szkolenie_tester\\Edgedriver\\msedgedriver.exe";
+                //System.setProperty("webdriver.edge.driver", pathE);
                 return new EdgeDriver();
                 //WebDriver driverEdge = new EdgeDriver();
                 //driverEdge.get("https://www.google.com");
