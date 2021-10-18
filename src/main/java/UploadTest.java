3b7ba0c7ec35dbe756f0185e5b8fbe30b34c95c1
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-10-18 08:43:09 +0200

    upload pliku przez sendkeys

---
+++ src/main/java/UploadTest.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UploadTest {
+
+    @Test
+    public void uploadFile() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
+        driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\Tomasz\\Downloads\\schlockenstein-rundweg.gpx");
+    }
+}
