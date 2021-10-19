018ffa08a31cdd4ac98427d93db45c96f7508e48
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-10-19 08:56:32 +0200

    klikanie prawym przyciskiem myszy

--- src/main/java/ActionsTest.java
+++ src/main/java/ActionsTest.java
@@ -10,23 +10,15 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 
-public class UploadTest {
+public class ActionsTest {
 
     @Test
-    public void uploadFile() throws IOException {
+    public void uploadFile()  {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
-      //  driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\Tomasz\\Downloads\\chlockenstein-rundweg.gpx");
-        //robienie screenshotów
-        int randomNumber = (int) (Math.random()*1000);
-        TakesScreenshot screenshot = (TakesScreenshot) driver;
-        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-        String fileName = "screen"+randomNumber+".png";
-        FileUtils.copyFile(srcFile, new File("src/test/resources/"+fileName));
-    // klikanie prawym przyciskiem myszy na stronie.
+      // klikanie prawym przyciskiem myszy na stronie.
         Actions actions = new Actions(driver);
         //actions.contextClick().perform(); //klika w dowolnym miejscu
         actions.contextClick(driver.findElement(By.id("myFile"))).perform();// konkretny webelement
