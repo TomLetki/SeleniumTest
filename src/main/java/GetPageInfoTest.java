3477868ba673d3e4e3b3f2d9fc9aae67c29b9ca0
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-10-20 08:10:43 +0200

    pobieranie adresu i tytułu strony

---
+++ src/main/java/GetPageInfoTest.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class GetPageInfoTest {
+
+    @Test
+    public void getInfo() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+       // System.out.println(driver.getPageSource());
+        driver.close();
+    }
+    }
