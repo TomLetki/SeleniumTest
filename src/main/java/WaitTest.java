8703f202a491a703f1c7c73f4f851b970899aa8b
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-11-04 09:35:05 +0100

    fluent wait + wyjątki

--- src/main/java/WaitTest.java
+++ src/main/java/WaitTest.java
@@ -1,11 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class WaitTest {
@@ -15,12 +20,21 @@ public class WaitTest {
     public void waitTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //Timeout do całego kodu.
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
         driver.findElement(By.id("clickOnMe")).click();
+
+
+      //  WebDriverWait wait = new WebDriverWait(driver, 10);
+       // wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+
         driver.findElement(By.cssSelector("p"));
-
-
     }
 }
