c29c84eaa44c3bada9b6a1c8330859d9dcf3bd44
Tomasz Letkiewicz <tk.letkiewicz@gmail.com>
2021-11-18 16:16:58 +0100

    Asercje: assertTrue i inne aseecje twarde

--- src/main/java/pl/testeroprogramowania/FirstTest.java
+++ src/main/java/pl/testeroprogramowania/FirstTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pl.testeroprogramowania.BaseTest;
 
 import java.io.File;
@@ -18,18 +19,24 @@ import java.util.List;
 public class FirstTest extends BaseTest {
     WebDriver driver;
 
-    @Test @Ignore
+    @Test
     public void firstTest(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
         driver.findElement(By.id("clickOnMe")).click();
-
-
         waitForElement(By.cssSelector("p"));
 
-        String paraText = driver.findElement(By.cssSelector("p")).getText();
-        Assert.assertEquals(paraText,"Dopiero się pojawiłem!");
+        WebElement para = driver.findElement(By.cssSelector("p"));
+        Assert.assertTrue(para.isDisplayed(), "Element is not displayed");
+        Assert.assertTrue(para.getText().startsWith("Dopiero"));
+        Assert.assertFalse(para.getText().startsWith("Pojawiłem"));
+        //Assert.assertEquals(para.isDisplayed(), true);
+        Assert.assertEquals(para.getText(), "Dopiero się pojawiłem!", "Teksty są jednakowe");
+
+        //String paraText = driver.findElement(By.cssSelector("p")).getText();
+        //Assert.assertEquals(paraText,"Dopiero się pojawiłem!");
+        //Assert.assertEquals(paraText, "Dopiero", "Teksty są różne");
         driver.quit();
     }
     @Test
